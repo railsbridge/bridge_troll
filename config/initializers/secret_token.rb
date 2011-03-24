e547a8f55f1ca194df5d40b9ab003449151bd8d5
Raphomet <Raphael.T.Lee@gmail.com>
2011-03-23 22:38:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridgeTroll::Application.config.secret_token = '4441a9a14f47540d141aeca769922985aca9a14a28c5c339701d0350368d93bbb6a63f1be32957ae7ee6f1305820d9ac6aefc8357ac2e143664f8298272de725'
