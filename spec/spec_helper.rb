802c2612bbc51bd6a8aaa8a57176279d349c8a7c
Kelly <defaultstring@gmail.com>
2011-10-16 13:46:25 -0700

    [spec] Adding in boilerplate spec helper so that we can run tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+require 'faker'
+require 'rake'
+
+Dir["#{File.dirname(__FILE__)}/helpers/**/*.rb"].each { |f| require f }
+
+module RSpec::Core
+  class ExampleGroup
+    include Capybara::DSL
+  end
+end
+
+Capybara.configure do |config|
+  config.default_driver = :selenium
+  config.default_selector = :css
+end
+
+RSpec.configure do |config|
+  config.include(Capybara, :type => :integration)
+  config.before :each do
+    ActiveRecord::Base.send(:descendants).each do |klass|
+      klass.destroy_all
+    end
+
+    load 'db/seeds.rb'
+  end
+end
