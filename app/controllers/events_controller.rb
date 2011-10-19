b9acc62a501851d2105bc71a8f8d3ffba52e82c6
Kelly <defaultstring@gmail.com>
2011-10-19 00:11:39 -0700

    [controller] Adding in event rerouting logicd

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,18 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:notice] = "Event successfully saved!"
+      raedirect_to event_path @event
+    else
+      redirect_to new_event_path
+    end
+  end
+
   def show
     @event = Event.find(params[:id], :include => [:registrations, :location])
   end
