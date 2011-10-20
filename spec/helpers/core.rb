eaf7be4c8e8cb20fe1da0f9f28a5b65ba58ca2ce
Kelly <defaultstring@gmail.com>
2011-10-19 21:40:24 -0700

    [spec] Adding in helpers

--- spec/helpers/core.rb
+++ spec/helpers/core.rb
@@ -6,6 +6,12 @@ module CoreHelper
     location = defaulted_option(options[:location], Location.first.name)
     select(location, :from => "event[location_id]")
 
+    start_time = defaulted_option(options[:start_time], Time.now)
+    fill_in "event[start_time]", :with => start_time
+
+    end_time = defaulted_option(options[:end_time], Time.now)
+    fill_in "event[end_time]", :with => end_time
+
     click_button "Create event"
     return Event.last
   end
