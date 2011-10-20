40bd6b4e952b5e371650a7942d7b67e16409dcf6
Kelly <defaultstring@gmail.com>
2011-10-19 20:01:48 -0700

    [spec] Some refactorin

---
+++ spec/helpers/core.rb
@@ -0,0 +1,17 @@
+module CoreHelper
+  def create_event(options = {})
+    visit new_event_path
+
+    name = defaulted_option(options[:name], "test")
+    fill_in "event[name]", :with => options[:name] || "test"
+
+    location = defaulted_option(options[:location], Location.first.name)
+    select(location, :from => "event[location_id]")
+
+    click_button "Create event"
+  end
+
+  def defaulted_option(option, default)
+    return !option.nil?? option : default
+  end
+end
--- spec/integrations/events/new_event_spec.rb
+++ spec/integrations/events/new_event_spec.rb
@@ -1,20 +1,18 @@
 require 'spec_helper'
 
 describe "Creating a new event" do
+  include CoreHelper
+
   it "should be able to create a new event" do
     old_events = Event.all
-    visit new_event_path
-    fill_in "event[name]", :with => "test"
-    click_button "Create event"
+    create_event
     old_events.length.should < Event.all.length
   end
 
   it "should be able to select a different location" do
     visit new_event_path
     new_location = Location.last.name
-    fill_in "event[name]", :with => "test"
-    select(new_location, :from => "event[location_id]")
-    click_button "Create event"
+    create_event({:location => new_location})
     Event.last.location.name.should == new_location
   end
 end
