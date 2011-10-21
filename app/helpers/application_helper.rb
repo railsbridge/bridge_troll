81a5dcf8be76c4bf3b0e58d1a7e4b0e3786bd192
Erik Pukinskis <erik@snowedin.net>
2011-10-21 00:04:12 -0700

    Added date_day_time helper to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
+  include ActiveSupport::Inflector
+
   def link_for(obj)
     link_to obj.to_s, url_for(obj)
+  end
+
+  def day_date_time(time)
+    time.strftime("%A %B ") + 
+    ordinalize(time.day) + 
+    time.strftime(" %Y at %l:%M") + 
+    time.strftime("%p").downcase
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,9 @@ describe ApplicationHelper do
     event.stub! :id => 3, :to_s => "hi", :new_record? => false
     helper.link_for(event).should == '<a href="/events/3">hi</a>'  
   end
+
+  it "should describe the time" do
+    start = Time.parse("October 20, 2011 11:56pm")
+    helper.day_date_time(start).should == "Thursday October 20th 2011 at 11:56pm"
+  end
 end
