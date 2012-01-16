4a2365506837e8f94c29c9daaf0d7e403abd932e
Sarah Mei <sarahmei@gmail.com>
2012-01-15 20:55:21 -0800

    Don't require ruby-debug anywhere but development. kkthx\!

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 BridgeTroll::Application.initialize!
 
 # Optionally require the debugger
-unless Rails.env == "production"
+if Rails.env == "development"
   require "ruby-debug" 
   Debugger.start
   Debugger.settings[:autoeval] = true
