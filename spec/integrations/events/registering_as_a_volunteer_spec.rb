0383315253637cfdbbaf37289c61bbd80675e5e8
Sarah Mei <sarahmei@gmail.com>
2012-01-15 16:11:47 -0800

    Add pending spec for volunteer skills

--- spec/integrations/events/registering_as_a_volunteer_spec.rb
+++ spec/integrations/events/registering_as_a_volunteer_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
-describe "An event one hour from now" do
-  it "should allow users to register as a volunteer" do
+describe "Volunteer registration" do
+  it "lets users register as a volunteer for an upcoming event" do
+    pending
     event = create_event
     user = create_user(:name => "Chipotle")
     sign_in(user)
     click_link event.name
     click_button "Volunteer"
+
+    within ".skills" do
+      check "Teaching"
+      check "Logistics"
+      click_button "Sign Me Up"
+    end
+
     within ".volunteers" do
       page.should have_content("Chipotle")
     end
