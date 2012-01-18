c0ce61ea7e8f33f7eb826dd95c45207190779b9f
Sarah Allen <sarah@ultrasaurus.com>
2012-01-17 19:11:38 -0800

    brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridgetroll::Application.config.secret_token = 'fa5da9ec833b3b9fe277a76a84c2f4982bb9dec82d18e5aa97fe25c6cbefdb5ae58894de0e530c1d951c8db3c1efa6f6f2a4ca2f534c7ffe8912e4535a0adacb'
