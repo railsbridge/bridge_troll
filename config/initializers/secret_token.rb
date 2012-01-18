030a048b114a37ba4508df0ddcc210632cd2b293
Judy Tuan <judytuna@gmail.com>
2012-01-17 19:34:54 -0800

    init commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridgetroll::Application.config.secret_token = 'fe60e775ae3acfad01b4bd0ffa25dde5e6a6fbadc6937d88f19aec9f67e7546f73522a23f11c5f87a7fd13b67131b2d550067b0ff05214783fa0748e0fd13eb3'
