692116cee9a1d50def8d6219b56fb48bbcd13759
Sarah Allen <sarah@ultrasaurus.com>
2012-01-17 19:53:41 -0800

    installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bridgetroll::Application.routes.draw do
   resources :events
 
+  root :to => "events#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
