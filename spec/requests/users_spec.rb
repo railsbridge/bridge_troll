fdacf5dcf2a7efe9750ec14db158daa0137a3c41
Nina R <nina.revko@gmail.com>
2012-02-06 23:32:57 -0800

    edited user spec for addition of skills

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,8 +5,6 @@ describe "existing user", :js => true do
   before(:each) do
     @user = Factory(:user)
     @user.confirm!
-
-    #@event = Factory(:event)
   end
 
   it "should see sign in link on the home page" do
@@ -30,56 +28,53 @@ describe "existing user", :js => true do
     page.should have_content("Signed in successfully")
     page.should have_link("Sign Out")
   end
-  
-  it "should signin and edit his/her settings" do
-    pending
-    visit new_user_session_path
 
-    fill_in "Email", :with => @user.email
-    fill_in "Password", :with => @user.password
-    click_button "Sign in"
+  describe "skills" do
+    before do
+      visit new_user_session_path
 
-    page.should have_content("Signed in successfully")
+      fill_in "Email", :with => @user.email
+      fill_in "Password", :with => @user.password
+      click_button "Sign in"
 
-    page.should have_content("Become Volunteer")
+      wait_until{page.has_content?("Signed in successfully")}
+    end
 
-    click_link "Become A Volunteer"
+    it "link to add skills should be present on the home page for logged in user" do
+      page.should have_link("Add Your Skills")
+    end
 
-    page.should have_content("specify your skills")
+    it "user should be able to add his/her skills" do
+      click_link "Add Your Skills"
 
-    check("Coordinating")
-    check("Teaching")
-    check("TAing")
-    check("Mentoring")
-    check("Childcaring")
-    check("Writing")
-    page.should have_content("Evangelizing")
-    page.should have_content("Other")
+      page.should have_content("My Incredible Powers")
 
-    click_button "Submit"
+      check "Teaching"
+      check "TAing"
+      check "Coordinating"
+      check "Childcaring"
+      check "Writing"
+      check "Hacking"
+      check "Designing"
+      page.should have_content("Evangelizing")
+      page.should have_content("Mentoring")
+      fill_in "Other", :with => "Speaking Spanish"
 
-    page.should have_content("Thanks for submitting your skills")
+      click_button "Submit"
 
-    page.should have_no_content("Become A Volunteer")
+      page.should have_content("Thanks for adding your skills")
 
-    @user.skills.should == ["Coordinating", "Teaching", "TAing", "Mentoring", "Hacking", "Designing", "Childcaring", "Writing"]
+      @user.skill_teaching.should be_true
+      @user.skill_taing.should be_true
+      @user.skill_coordinating.should be_true
+      @user.skill_childcaring.should be_true
+      @user.skill_writing.should be_true
+      @user.skill_hacking.should be_true
+      @user.skill_designing.should be_true
+      @user.skill_evangelizing.should be_false
+      @user.skill_mentoring.should be_false
+      @user.skill_other.should == "Speaking Spanish"
 
-    page.should have_content("Update your skills")
-
-    click_link "Update your skills"
-
-    page.should have_content("specify your skills")
-
-    uncheck("Hacking")
-    uncheck("Designing")
-
-    click_button "Submit"
-
-    page.should have_content("Thanks for submitting your skills")
-
-    @user.skills.should == ["Coordinating", "Teaching", "TAing", "Mentoring", "Childcaring", "Writing"]
-
-
+    end
   end
-
 end
