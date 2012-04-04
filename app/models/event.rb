ab141da8749da7dfdf8784394a50c61ae9db28e5
Mary Wang <mmmmqc@gmail.com>
2012-04-03 21:12:59 -0700

    check presence of event title, location, date. add link to locations page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :location
   has_many :volunteerRsvps
   has_many :users, :through => :volunteerRsvps
+  validates_presence_of :title
+  validates_presence_of :location
+  validates_presence_of :date
 end
