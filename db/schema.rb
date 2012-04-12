27a94e09fbf508830b500e5c94a133e48400b730
Sarah Allen <sarah@ultrasaurus.com>
2012-04-11 20:19:51 -0700

    adding schema file for last migration that added user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120313184512) do
+ActiveRecord::Schema.define(:version => 20120410060636) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20120313184512) do
     t.boolean  "windows"
     t.boolean  "linux"
     t.string   "other"
+    t.string   "name"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
