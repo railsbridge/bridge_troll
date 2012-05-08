e05052fb2df1b82c20e2570b28eb991cc0144c36
sibilant_threads <sibilant.threads@gmail.com>
2012-05-07 20:26:21 -0700

    took out if...else statement--controller always redirects to @event after saving volunteer_rsvp

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -77,12 +77,9 @@ class EventsController < ApplicationController
      
      @event = Event.find(params[:id])
      @rsvp = @event.volunteer!(current_user)
-      
-     if @rsvp.persisted? and @rsvp.valid?
-       redirect_to @event, notice: 'Thanks for volunteering!'
-     else
-       redirect_to "/events", notice: "Oops!  Something went wrong.  Check to see if you're signed up or try again."
-     end
+
+     redirect_to @event, notice: 'Thanks for volunteering!'
+     
   end
   
   def unvolunteer
