ec15548718e85054bf0315b5dc1ab3012bd85cf3
Shauna <slhaines@gmail.com>
2012-05-22 21:09:22 -0700

    Only include events today or later ("upcoming")

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+# binding.pry    
+    @events = Event.upcoming
 
     respond_to do |format|
       format.html # index.html.erb
