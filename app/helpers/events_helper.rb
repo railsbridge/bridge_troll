98f9fb02e5d65cb65fe0ee10989d2fccb3be7462
John Forbes <john.forbes@yahoo.com>
2012-12-23 10:50:04 -0800

    added the four volunteer preferences sections teach-only, ta-only, either, neither

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -16,21 +16,38 @@ module EventsHelper
     @skills.join(', ')
   end
 
-  def teachers(volunteers)
+  def teachers_count(volunteers)
     count = 0
     volunteers.each do |volunteer|
-      count += volunteer.teaching ? 1 : 0
+      count += volunteer.teaching && !volunteer.taing ? 1 : 0
     end
     count
   end
 
-  def tas(volunteers)
+  def tas_count(volunteers)
     count = 0
     volunteers.each do |volunteer|
-      count += volunteer.taing ? 1 : 0
+      count += volunteer.taing && !volunteer.teaching ? 1 : 0
     end
     count
   end
+
+  def teach_or_ta_count(volunteers)
+    count = 0
+    volunteers.each do |volunteer|
+      count += volunteer.taing && volunteer.teaching ? 1 : 0
+    end
+    count
+  end
+
+  def not_teach_or_ta_count(volunteers)
+    count = 0
+    volunteers.each do |volunteer|
+      count += !volunteer.taing && !volunteer.teaching ? 1 : 0
+    end
+    count
+  end
+
 
   def organizer_title
    @event.organizers.length > 1 ? "Organizers:" : "Organizer:"
@@ -38,6 +55,40 @@ module EventsHelper
 
   def organizer_list
     @event.organizers.length == 0 ?  [{:name => "No Organizer Assigned"}] : @event.organizers
+  end
+
+  def volunteer_display(volunteer, *attributes)
+    volunteer_class = ""
+    if attributes.length == 2
+      if volunteer.send(attributes[0]) && volunteer.send(attributes[1])
+        volunteer_class = "both"
+      end
+    end
+
+    if attributes.length == 1 && attributes[0] == "teaching"
+      if volunteer.send(attributes[0]) && !volunteer.taing
+        volunteer_class = "teach"
+      end
+    end
+
+    if attributes.length == 1 && attributes[0] == "taing"
+      if volunteer.send(attributes[0]) && !volunteer.teaching
+        volunteer_class = "ta"
+      end
+    end
+
+    if attributes.length == 0
+      if !volunteer.taing && !volunteer.teaching
+        volunteer_class = "none"
+      end
+    end
+    get_tag volunteer, volunteer_class unless volunteer_class.empty?
+  end
+
+  private
+
+  def get_tag(volunteer, volunteer_class)
+    content_tag "li","#{volunteer.name} - #{volunteer.email}", :class => volunteer_class
   end
 
 end
