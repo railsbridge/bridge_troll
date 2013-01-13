bfd472c4cfbf1bcdd755620d392cc0754bf809f2
Sarah Mei <sarahmei@gmail.com>
2013-01-13 01:54:50 -0800

    Delete existing events & associated models so that they won't be invalid in the brave new world

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112231445) do
+ActiveRecord::Schema.define(:version => 20130113094917) do
 
   create_table "event_organizers", :force => true do |t|
     t.integer  "user_id"
