9f3d510ff869754bc0862434bf47ae92a33d4059
eanakashima <e.a.nakashima@gmail.com>
2013-01-26 17:55:48 -0800

    [#42866617] Change "lots o' minutes" to 2.weeks, bro.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -109,7 +109,7 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
 
-  config.timeout_in = 20160.minutes # Lillie wanted 'forever' but we compromised at '2 weeks'
+  config.timeout_in = 2.weeks # Lillie wanted 'forever' but we compromised at '2 weeks'
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
