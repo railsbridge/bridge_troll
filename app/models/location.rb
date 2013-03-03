55a09edc64a76a7f3759701137553bdb8da7b6c9
Travis Grathwell <tjgrathwell@gmail.com>
2013-03-03 14:02:08 -0800

    Fix failing tests by disabling gmaps api requests in test

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   has_many :events
   validates_presence_of :name, :address_1, :city
-  acts_as_gmappable
+  acts_as_gmappable(process_geocoding: !Rails.env.test?)
 
   def gmaps4rails_address
     "#{self.address_1}, #{self.city}, #{self.state}, #{self.zip}" 
