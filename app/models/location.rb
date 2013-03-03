5f2f38774bc8c926d5b8c2e1b0eea790188d7e1f
Lillie Chilen <lillie.chilen@gmail.com>
2013-03-02 16:42:05 -0800

    Event and location detail show map

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :events
   validates_presence_of :name, :address_1, :city
+  acts_as_gmappable
+
+  def gmaps4rails_address
+    "#{self.address_1}, #{self.city}, #{self.state}, #{self.zip}" 
+  end
 end
