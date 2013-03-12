755d93ce59e7fbcf39281c54136645c5cb44e53d
K. Stewart <ignitiant@gmail.com>
2013-03-12 12:08:31 -0700

    Volunteer_emails spec: sign in as admin
    The app won't let you see the list of volunteer's emails unless
    you're an admin.  As it should be.
    https://www.pivotaltracker.com/story/show/45012157

--- spec/requests/event_volunteer_emails_request_spec.rb
+++ spec/requests/event_volunteer_emails_request_spec.rb
@@ -5,6 +5,9 @@ describe "the volunteer email page" do
     event = create(:event, :location_id => nil, :title => 'Volunteer Magnet')
     create(:event_session, event: event, starts_at: 1.day.from_now, ends_at: 2.days.from_now)
 
+    organizer = create(:user)
+    rsvp1 = create(:rsvp, user: organizer, event: event, role_id: Role::ORGANIZER)
+
     guy1 = create(:user, :email => 'guy1@email.com')
     rsvp1 = create(:rsvp, user: guy1, event: event, role_id: Role::VOLUNTEER)
     guy2 = create(:user, :email => 'guy2@email.com')
@@ -15,6 +18,7 @@ describe "the volunteer email page" do
     volunteers = [guy1, guy2]
     event.volunteers = volunteers
 
+    sign_in_as(organizer)
     visit volunteer_emails_event_path(event)
 
     page.should have_content(guy1.email)
