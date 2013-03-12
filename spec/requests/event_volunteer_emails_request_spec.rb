8ded66709c4ccc5857a61f73c171f170acf160ab
K. Stewart <ignitiant@gmail.com>
2013-03-12 11:48:05 -0700

    Fixing specs for volunteer_email
    reconciles spec with rsvp validations
    https://github.com/railsbridge/bridge_troll/pull/110

--- spec/requests/event_volunteer_emails_request_spec.rb
+++ spec/requests/event_volunteer_emails_request_spec.rb
@@ -6,7 +6,10 @@ describe "the volunteer email page" do
     create(:event_session, event: event, starts_at: 1.day.from_now, ends_at: 2.days.from_now)
 
     guy1 = create(:user, :email => 'guy1@email.com')
+    rsvp1 = create(:rsvp, user: guy1, event: event, role_id: Role::VOLUNTEER)
     guy2 = create(:user, :email => 'guy2@email.com')
+    rsvp2 = create(:rsvp, user: guy2, event: event, role_id: Role::VOLUNTEER)
+
     guy3 = create(:user, :email => 'guy3@email.com')
 
     volunteers = [guy1, guy2]
