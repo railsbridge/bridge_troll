f40097e391e7e631ba964246360ca79185f1dc2e
Travis Grathwell <tjgrathwell@gmail.com>
2013-05-12 23:16:56 -0700

    Remove reference to not-yet-existing /templates dir

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require underscore
 //= require backbone
 //= require bridgetroll
-//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
