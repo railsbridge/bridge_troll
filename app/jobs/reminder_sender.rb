a3959ada57722730384fa4f7462db187f4fdfd59
Travis Grathwell <tjgrathwell@gmail.com>
2013-05-22 19:05:12 -0700

    Add city to location displayed in event index

--- app/jobs/reminder_sender.rb
+++ app/jobs/reminder_sender.rb
@@ -7,7 +7,7 @@ class ReminderSender
 
   def self.remind_attendees_for(event)
     due_reminders = event.rsvps.confirmed.where(:reminded_at => nil)
-    puts "Sending #{due_reminders.count} reminders for #{event.title}..."
+    puts "Sending #{due_reminders.count} reminders for #{event.title}..." unless Rails.env.test?
     due_reminders.find_each do |rsvp|
       RsvpMailer.reminder(rsvp).deliver
       rsvp.update_attributes(reminded_at: Time.now)
