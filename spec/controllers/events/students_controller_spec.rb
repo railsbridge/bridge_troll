73f8888456b05b96e183107ef50c72dd64681af6
Travis Grathwell <tjgrathwell@gmail.com>
2013-05-31 12:59:56 -0700

    Fix students controller test to respect new visibility restrictions

--- spec/controllers/events/students_controller_spec.rb
+++ spec/controllers/events/students_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Events::StudentsController do
   let(:student_rsvp) { create(:student_rsvp) }
   let(:event) { student_rsvp.event }
+  let(:organizer) { create(:user) }
+
+  before do
+    event.organizers << organizer
+    sign_in organizer
+  end
 
   describe '#index' do
     it 'has the right headers' do
