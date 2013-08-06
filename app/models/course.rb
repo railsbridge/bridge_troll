db9cb545dc8f412e7bc2a0c690723db0470d01b1
Travis Grathwell <tjgrathwell@gmail.com>
2013-08-05 20:56:57 -0700

    Add link to event level description page

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,7 +5,7 @@ class Course < ActiveHash::Base
       id: 1,
       name: 'RAILS',
       title: 'Ruby on Rails',
-      description: 'This is a Ruby on Rails event. The focus will be on developing functional web apps and programming in Ruby.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org">docs.railsbridge.org</a>',
+      description: 'This is a Ruby on Rails event. The focus will be on developing functional web apps and programming in Ruby.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org">docs.railsbridge.org</a>.',
       levels: [
         {
           level: 1,
@@ -62,7 +62,7 @@ class Course < ActiveHash::Base
       id: 2,
       name: 'FRONTEND',
       title: 'Front End',
-      description: 'This is a Front End workshop. The focus will be on designing web apps with HTML and CSS.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org/frontend">docs.railsbridge.org/frontend</a>',
+      description: 'This is a Front End workshop. The focus will be on designing web apps with HTML and CSS.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org/frontend">docs.railsbridge.org/frontend</a>.',
       levels: [
         {
           level: 1,
