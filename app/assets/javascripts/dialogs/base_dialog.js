6eb6f395311c78e6976da1b513211eb89779472c
Travis Grathwell <tjgrathwell@gmail.com>
2013-10-05 19:51:37 -0700

    Section arranger: focus on btns in modal so they are closable with 'esc'
    Change focused .btn style to maintain original background rather than going grey

--- app/assets/javascripts/dialogs/base_dialog.js
+++ app/assets/javascripts/dialogs/base_dialog.js
@@ -5,6 +5,10 @@ Bridgetroll.Dialogs.Base = (function () {
       $(Bridgetroll.modalContainerSelector).append(this.el);
       $(this.el).modal();
 
+      this.$el.on('shown', function () {
+        $(this).find('.btn').focus();
+      });
+
       $(this.el).on('hidden', _.bind(function () {
         this.destroy();
       }, this));
