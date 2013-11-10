ed790c49717d25a7eadd6025f4baf292b831b354
Travis Grathwell <tjgrathwell@gmail.com>
2013-11-09 21:02:31 -0800

    Canonicalize installfest/docs URLs in student/vol details
    Simplify "role_id ==" statements

--- app/models/event.rb
+++ app/models/event.rb
@@ -276,10 +276,10 @@ All students need to bring their own laptop and powercord.
 
 Since bandwidth is usually at a premium at the Installfest, please download RailsInstaller (for PCs and most Mac installations) or XCode (if you're going that route).
 
-You can find more information on what to download by getting started with the Installfest instructions: http://installfest.railsbridge.org/installfest/
+You can find more information on what to download by getting started with the Installfest instructions: http://installfest.railsbridge.org
   END
 
   DEFAULT_VOLUNTEER_DETAILS = <<-END
-Be sure to review the curriculum before the workshop. We have several curricula available at http://docs.railsbridge.org/docs/.
+Be sure to review the curriculum before the workshop. We have several curricula available at http://docs.railsbridge.org.
   END
 end
