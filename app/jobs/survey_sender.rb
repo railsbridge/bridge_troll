9453d73be2db94378b83b4af619e611403e7acee
Travis Grathwell <tjgrathwell@gmail.com>
2014-02-01 21:17:44 -0800

    Only send surveys to attendees who checked in to some session

--- app/jobs/survey_sender.rb
+++ app/jobs/survey_sender.rb
@@ -1,6 +1,6 @@
 class SurveySender
   def self.send_surveys(event)
-    attendee_rsvps = event.attendee_rsvps
+    attendee_rsvps = event.attendee_rsvps.where('checkins_count > 0')
     attendee_rsvps.each do |rsvp|
       SurveyMailer.notification(rsvp).deliver
     end
--- spec/jobs/survey_sender_spec.rb
+++ spec/jobs/survey_sender_spec.rb
@@ -3,13 +3,22 @@ require 'spec_helper'
 describe SurveySender do
   describe ".send_surveys" do
     let(:event) { create(:event, student_rsvp_limit: 1) }
-    let!(:rsvp) { create(:volunteer_rsvp, event: event) }
-    let!(:student_rsvp) { create(:student_rsvp, event: event) }
+    before do
+      session = event.event_sessions.first
 
-    it "sends survey emails to all the attendees" do
+      volunteer_rsvp = create(:volunteer_rsvp, event: event)
+      create(:rsvp_session, rsvp: volunteer_rsvp, event_session: session, checked_in: true)
+
+      student_rsvp = create(:student_rsvp, event: event)
+      create(:rsvp_session, rsvp: student_rsvp, event_session: session, checked_in: true)
+
+      no_show_rsvp = create(:volunteer_rsvp, event: event)
+    end
+
+    it "sends survey emails to all the attendees who checked in" do
       expect {
         SurveySender.send_surveys(event)
-      }.to change(ActionMailer::Base.deliveries, :count).by(event.rsvps.count)
+      }.to change(ActionMailer::Base.deliveries, :count).by(2)
     end
   end
 end
