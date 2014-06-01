098e252b37388f6137484cbaa364ce6f465b1c9e
Travis Grathwell <tjgrathwell@gmail.com>
2014-05-31 20:07:50 -0700

    Fix mostly postgres-specific troubles caused by Rails 4 migration
    * Avoid deadly "group" + "find_each" combo by making the
      user's page just fetch everything outright without find_each
    * Add 'event' to the group clause for a query that wanted it to be there
    * Remove reference to attr_accessible from a migration
      so it will actually work if someone runs the migrations fresh

--- db/migrate/20140319070914_convert_meetup_ids_to_authentications.rb
+++ db/migrate/20140319070914_convert_meetup_ids_to_authentications.rb
@@ -4,7 +4,6 @@ class ConvertMeetupIdsToAuthentications < ActiveRecord::Migration
   end
 
   class Authentication < ActiveRecord::Base
-    attr_accessible :provider, :uid, :user_id
     validates_uniqueness_of :provider, scope: [:uid]
 
     belongs_to :user, inverse_of: :authentications
