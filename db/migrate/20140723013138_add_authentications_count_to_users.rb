7532786e672c83b53dfa9811228f36bca88cec4b
Travis Grathwell <tjgrathwell@gmail.com>
2014-07-24 23:50:10 -0700

    Split authentications_count migration into 'up' and 'down'

--- db/migrate/20140723013138_add_authentications_count_to_users.rb
+++ db/migrate/20140723013138_add_authentications_count_to_users.rb
@@ -6,12 +6,16 @@ class AddAuthenticationsCountToUsers < ActiveRecord::Migration
     belongs_to :user, counter_cache: true
   end
 
-  def change
+  def up
     add_column :users, :authentications_count, :integer
 
     User.reset_column_information
     User.select(:id).find_each do |u|
       User.reset_counters u.id, :authentications
     end
+  end
+
+  def down
+    remove_column :users, :authentications_count
   end
 end
