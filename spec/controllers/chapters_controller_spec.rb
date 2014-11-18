aa6b67088e6032c768a7c2da034572ba89dc1c71
Travis Grathwell <tjgrathwell@gmail.com>
2014-11-17 22:37:09 -0800

    Don't crash if a non-authed user happens to arrive on a chapter show page

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -7,18 +7,31 @@ describe ChaptersController do
 
   describe "permissions" do
     context "a user that is not logged in" do
+      context "when rendering views" do
+        render_views
+        it "can see the index page" do
+          get :index
+          response.should be_success
+        end
+
+        it "can see the show page" do
+          get :show, id: @chapter.id
+          response.should be_success
+        end
+      end
+
       it "should not be able to create a new chapter" do
         get :new
         response.should redirect_to(new_user_session_path)
       end
 
       it "should not be able to edit a chapter" do
-        get :edit, {id: @chapter.id}
+        get :edit, id: @chapter.id
         response.should redirect_to(new_user_session_path)
       end
 
       it "should not be able to delete a chapter" do
-        delete :destroy, {id: @chapter.id}
+        delete :destroy, id: @chapter.id
         response.should redirect_to(new_user_session_path)
       end
     end
