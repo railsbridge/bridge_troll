72d65016c84742d6ce9e35362bff2bb03cf526ef
Travis Grathwell <tjgrathwell@gmail.com>
2014-11-21 22:07:20 -0800

    Show password editing field on settings page if it was ever set

--- spec/features/edit_account_details_spec.rb
+++ spec/features/edit_account_details_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe "Profile" do
+  let(:new_password) { "Blueberry23" }
+
   before do
     @user = create(:user, password: "MyPassword")
     sign_in_as(@user)
@@ -17,40 +19,53 @@ describe "Profile" do
     @user.gender.should eq("Wizard")
   end
 
+  it "allows password to be changed even if an authentication is present" do
+    @user.authentications.create(provider: 'github', uid: 'abcdefg')
+    @user.reload
+
+    visit edit_user_registration_path
+    fill_in("Password", match: :first, with: new_password)
+    fill_in("Password confirmation", with: new_password)
+    fill_in("Current password", with: "MyPassword")
+    click_button "Update"
+
+    @user.reload.valid_password?(new_password).should be true
+  end
+
   context "when changing your password" do
     it "is successful when password matches confirmation" do
-      fill_in("Password", match: :first, with: "Blueberry23")
-      fill_in("Password confirmation", with: "Blueberry23")
+      fill_in("Password", match: :first, with: new_password)
+      fill_in("Password confirmation", with: new_password)
       fill_in("Current password", with: "MyPassword")
       click_button "Update"
 
-      @user.reload.valid_password?("Blueberry23").should be true
+      @user.reload.valid_password?(new_password).should be true
     end
 
     it "is unsuccessful when password and confirmation don't match" do
-      fill_in("Password", match: :first, with: "Blueberry23")
-      fill_in("Password confirmation", with: "blueberry23")
+      fill_in("Password", match: :first, with: new_password)
+      fill_in("Password confirmation", with: new_password.swapcase)
       fill_in("Current password", with: "MyPassword")
       click_button "Update"
 
-      @user.reload.valid_password?("Blueberry23").should be false
+      @user.reload.valid_password?(new_password).should be false
     end
 
     it "is unsuccessful when current password not provided" do
-      fill_in("Password", match: :first, with: "Blueberry23")
-      fill_in("Password confirmation", with: "Blueberry23")
+      fill_in("Password", match: :first, with: new_password)
+      fill_in("Password confirmation", with: new_password)
       click_button "Update"
 
       @user.reload.valid_password?("Blueberry23").should be false
     end
 
     it "is unsuccessful when current password is incorrect" do
-      fill_in("Password", match: :first, with: "Blueberry23")
-      fill_in("Password confirmation", with: "Blueberry23")
+      fill_in("Password", match: :first, with: new_password)
+      fill_in("Password confirmation", with: new_password)
       fill_in("Current password", with: "SomeOtherPassword")
       click_button "Update"
 
-      @user.reload.valid_password?("Blueberry23").should be false
+      @user.reload.valid_password?(new_password).should be false
     end
   end
 
