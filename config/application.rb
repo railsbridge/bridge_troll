928526063de41c86eb2ea2257525120835ec4918
Travis Grathwell <tjgrathwell@gmail.com>
2014-11-23 20:28:40 -0800

    Fix config issue causing JS to not be minified in production
    Since upgrading to Rails 4, the config.assets.compress
    option is deprecated in favor of config.assets.js_compressor.
    So we haven't been minifying our JS since May. whoops
    
    Refreshed the config/* files to look more like they should
    for a Rails 4.1 app, including secrets.yml etc.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 Bundler.require(:default, Rails.env)
 
@@ -15,16 +14,6 @@ module Bridgetroll
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
@@ -32,23 +21,6 @@ module Bridgetroll
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-    
-    # step 4 of configuring devise
-    #   4. If you are deploying Rails 3.1 on Heroku, you may want to force your
-    #   application to not access the DB or load models when precompiling your assets.
-    config.assets.initialize_on_precompile = false
 
     # set default from address across
     ActionMailer::Base.default :from => 'RailsBridge <troll@bridgetroll.org>'
