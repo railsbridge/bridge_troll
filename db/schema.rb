d6ac5952a9b6d29cacbb5eadbf28aa9601ecda8c
Travis Grathwell <tjgrathwell@gmail.com>
2015-03-26 20:32:35 -0700

    Add proper foreign key relationships to all foreign keys
    (except rsvps.user_id, because it's polymorphic)

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150511025307) do
-=======
-ActiveRecord::Schema.define(version: 20150317000242) do
->>>>>>> add volunteer rsvp limit to events
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id"
@@ -262,4 +258,25 @@ ActiveRecord::Schema.define(version: 20150317000242) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  add_foreign_key "authentications", "users"
+  add_foreign_key "chapter_leaderships", "chapters"
+  add_foreign_key "chapter_leaderships", "users"
+  add_foreign_key "chapters_users", "chapters"
+  add_foreign_key "chapters_users", "users"
+  add_foreign_key "dietary_restrictions", "rsvps"
+  add_foreign_key "event_email_recipients", "event_emails"
+  add_foreign_key "event_email_recipients", "rsvps", column: "recipient_rsvp_id"
+  add_foreign_key "event_emails", "events"
+  add_foreign_key "event_emails", "users", column: "sender_id"
+  add_foreign_key "event_sessions", "events"
+  add_foreign_key "events", "locations"
+  add_foreign_key "external_events", "chapters"
+  add_foreign_key "locations", "chapters"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "rsvp_sessions", "event_sessions"
+  add_foreign_key "rsvp_sessions", "rsvps"
+  add_foreign_key "rsvps", "events"
+  add_foreign_key "rsvps", "sections"
+  add_foreign_key "sections", "events"
+  add_foreign_key "surveys", "rsvps"
 end
