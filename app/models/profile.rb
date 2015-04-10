b61b14b250a763278ba7e67124bb0a99ded0ff92
Travis Grathwell <tjgrathwell@gmail.com>
2015-04-09 23:12:47 -0700

    Validate the format of github_username
    Per GitHub, "Username may only contain alphanumeric characters or
    single hyphens, and cannot begin or end with a hyphen"

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,8 @@ class Profile < ActiveRecord::Base
 
   belongs_to :user, inverse_of: :profile
 
+  validates_format_of :github_username, with: /\A([a-z0-9-]+-)*[a-z0-9]+\Z/i, allow_blank: true
+
   validates_presence_of :user
   validates_uniqueness_of :user_id
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -4,5 +4,19 @@ describe Profile do
   it { should belong_to(:user) }
   it { should validate_presence_of(:user) }
   it { should validate_uniqueness_of(:user_id) }
+
+  it 'validates the format of github_username' do
+    def prof(github_username)
+      Profile.new(github_username: github_username)
+    end
+
+    prof('-robocop').should have(1).errors_on(:github_username)
+    prof('coborop-').should have(1).errors_on(:github_username)
+    prof('i have spaces').should have(1).errors_on(:github_username)
+    prof('http://github.com/username').should have(1).errors_on(:github_username)
+
+    prof('foo-BAR-91').should have(0).errors_on(:github_username)
+    prof(nil).should have(0).errors_on(:github_username)
+  end
 end
 
