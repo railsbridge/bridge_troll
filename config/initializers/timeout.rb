62450d2904814c2393550eb9f3100ee1bbc3cc2f
Travis Grathwell <tjgrathwell@gmail.com>
2015-09-06 02:37:04 -0700

    Don't let puma preload the app if there's only one worker process
    It seems like some race condition causes one or both of the worker
    processes to be wedged on start. This can manifest after a deploy,
    or when Heroku does its daily dyno restart. The puma config now
    specifies not to `preload_app!` if there's only one worker process,
    because it provides no memory benefit in that situation anyway.
    
    We may need to revisit this decision if we ever start actually
    running more than one worker on each dyno.
    
    Decrease Rack::Timeout timeout to 20 seconds

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,3 +1,3 @@
 if Rails.env.production?
-  Rack::Timeout.timeout = 25 # seconds
+  Rack::Timeout.timeout = 20 # seconds
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,20 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+worker_count = Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers worker_count
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-preload_app!
+unless worker_count == 1
+  preload_app!
+end
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+unless worker_count == 1
+  on_worker_boot do
+    # Worker specific setup for Rails 4.1+
+    # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+    ActiveRecord::Base.establish_connection
+  end
 end
