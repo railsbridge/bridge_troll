71d11ed93f537ccbca6abe99c474ee529e83465e
Travis Grathwell <tjgrathwell@gmail.com>
2015-11-21 21:55:53 -0800

    Send surveys automatically, one day after the event

--- app/jobs/survey_sender.rb
+++ app/jobs/survey_sender.rb
@@ -1,5 +1,17 @@
 class SurveySender
+  FIRST_AUTO_SEND_DATE = Date.parse('2015-11-15')
+
+  def self.send_all_surveys
+    Event.where('ends_at > ?', FIRST_AUTO_SEND_DATE)
+      .where('ends_at < ?', 1.day.ago)
+      .where('survey_sent_at IS NULL').each do |event|
+      self.send_surveys(event)
+    end
+  end
+
   def self.send_surveys(event)
+    return if event.survey_sent_at.present?
+
     attendee_rsvps = event.attendee_rsvps.where('checkins_count > 0')
     attendee_rsvps.each do |rsvp|
       SurveyMailer.notification(rsvp).deliver_now
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,6 +1,13 @@
-desc "This task is called by the Heroku scheduler add-on"
-task :send_reminders => :environment do
+desc "Send reminders for upcoming events within the reminder window"
+task send_reminders: :environment do
   puts "Sending reminder emails..."
   ReminderSender.send_all_reminders
+  puts "...done."
+end
+
+desc "Send surveys for all past events"
+task send_surveys: :environment do
+  puts "Sending surveys..."
+  SurveySender.send_all_surveys
   puts "...done."
 end
