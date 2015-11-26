d834c8e85aabc365b2b6d9d2b7cdb790ae7ffff4
Travis Grathwell <tjgrathwell@gmail.com>
2015-11-25 23:03:17 -0600

    Add example_status_persistence_file_path for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 
+  config.example_status_persistence_file_path = "./spec/examples.txt"
+
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended.
