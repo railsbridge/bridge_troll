e984c3ec32a6a1dd59ccb6dc47706bd7f143749d
Travis Grathwell <tjgrathwell@gmail.com>
2015-11-27 23:35:46 -0800

    Add "null: false" constraints to timestamps fields that are missing them
    Backfill missing survey timestamps with a simulated timestamp of 22:22:22 UTC
    a day or two after the event had passed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127025203) do
+ActiveRecord::Schema.define(version: 20151128071249) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "dietary_restrictions", force: :cascade do |t|
@@ -230,8 +230,8 @@ ActiveRecord::Schema.define(version: 20151127025203) do
     t.text     "bad_things"
     t.text     "other_comments"
     t.integer  "recommendation_likelihood"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "users", force: :cascade do |t|
