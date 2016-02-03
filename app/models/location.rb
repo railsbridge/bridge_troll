3a436d256e0cf1ed40453be5de7ed8e38ffdf31c
Travis Grathwell <tjgrathwell@gmail.com>
2016-02-02 23:35:11 -0800

    Fix flaky most_recent_event_date test
    The result of event.starts_at.strftime(...) would differ
    depending on whether Time.zone had ever been set,
    which currently mostly happens when rendering events.
    
    Hence, a testrun with just the location_spec would not fail,
    but if any event feature tests ran before it, they might
    set the zone and cause the date to render differently.
    
    Forcing most_recent_event_date to always respect the zone
    solves this problem.

--- app/models/location.rb
+++ app/models/location.rb
@@ -76,7 +76,8 @@ class Location < ActiveRecord::Base
   def most_recent_event_date
     relevant_events = (events + event_sessions.map(&:event)).compact
     if relevant_events.present?
-      relevant_events.sort_by { |e| e.starts_at }.last.starts_at.strftime("%b %d, %Y")
+      event = relevant_events.sort_by { |e| e.starts_at }.last
+      event.starts_at.in_time_zone(event.time_zone).strftime("%b %d, %Y")
     else
       "No events found."
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,7 @@ RSpec.configure do |config|
 
   config.before(:each) do
     WebMock.disable_net_connect!(allow_localhost: true)
+    Time.zone = 'UTC'
   end
 
   config.include Devise::TestHelpers, type: :controller
