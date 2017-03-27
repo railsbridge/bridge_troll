5ffa3bf97e44de0e03d9460c5518a2b1b1253132
Travis Grathwell <tjgrathwell@gmail.com>
2017-03-26 22:23:41 -0700

    Backfill feature spec for answering custom question
    Move CoC feature tests into their own file
    Delete a couple feature tests that were only testing
    presence/absence of elements (no behavior)

--- spec/features/events/event_rsvp_request_spec.rb
+++ spec/features/events/event_rsvp_request_spec.rb
@@ -2,25 +2,23 @@ require 'rails_helper'
 
 describe 'creating or editing an rsvp' do
   context "for a teaching event" do
-    let(:no_preference_text) { 'No preference' }
+    def fill_in_valid_volunteer_details
+      fill_in "rsvp_subject_experience", with: "I have some subject experience"
+      fill_in "rsvp_teaching_experience", with: "I have some teaching experience"
+      choose @course.levels[0][:title]
+    end
+
     let(:chapter) { create(:chapter) }
     before do
       @event = create(:event, chapter: chapter)
       @user = create(:user)
       @course = @event.course
-      @course_js = create(:course, name: "JAVASCRIPT", title: "Intro to JavaScript")
-      @course_fe = create(:course, name: "FRONTEND", title: "Front End")
       sign_in_as @user
     end
 
     context "given a new volunteer rsvp" do
       before do
         visit volunteer_new_event_rsvp_path(@event)
-      end
-
-      it "should not show checkboxes for events with only one session" do
-        expect(@event.event_sessions.length).to eq(1)
-        expect(page).not_to have_content(@event.event_sessions.first.name)
       end
 
       it "allows user to toggle childcare info with the needs_childcare button", js: true do
@@ -36,19 +34,9 @@ describe 'creating or editing an rsvp' do
         expect(page).to have_field('rsvp_childcare_info', visible: false)
       end
 
-      it "should show option for any class level" do
-        expect(page).to have_content no_preference_text
-      end
-
-      it "should have a code of conduct checkbox" do
-        expect(page).to have_unchecked_field('coc')
-      end
-
       context "with a valid RSVP" do
         before do
-          fill_in "rsvp_subject_experience", with: "asdfasdfasdfasd"
-          fill_in "rsvp_teaching_experience", with: "asdfasdfasdfasd"
-          choose @course.levels[0][:title]
+          fill_in_valid_volunteer_details
         end
 
         it "should allow the user to update their gender" do
@@ -66,46 +54,6 @@ describe 'creating or editing an rsvp' do
           }.to change { @user.regions.count }.by(1)
           visit edit_event_rsvp_path(@event, Rsvp.last)
           expect(page.find("#affiliate_with_region").value).to eq("1")
-        end
-      end
-
-      describe 'code of conduct' do
-        let(:coc_text) { 'I accept the Code of Conduct' }
-
-        context 'for new records', js: true do
-          it "requires code of conduct to be checked, and preserves checked-ness on error" do
-            expect(page).to have_content(coc_text)
-
-            expect(page).to have_button 'Submit', disabled: true
-            expect(page).to have_unchecked_field('coc')
-            check('coc')
-            expect(page).to have_button 'Submit', disabled: false
-
-            click_on 'Submit'
-
-            expect(page).to have_css('#error_explanation')
-            expect(page).to have_checked_field('coc')
-          end
-        end
-
-        context 'for existing records' do
-          let(:rsvp) { create(:rsvp, user: @user) }
-
-          it 'is not shown' do
-            visit edit_event_rsvp_path rsvp.event, rsvp
-            expect(page).to have_no_content(coc_text)
-          end
-        end
-
-        context 'when the organization has a different code of conduct' do
-          let(:organization) do
-            create(:organization, name: 'CoolBridge', code_of_conduct_url: 'http://example.com/coc')
-          end
-          let(:chapter) { create(:chapter, organization: organization) }
-
-          it 'shows a custom code of conduct URL' do
-            expect(page.find('label[for=coc] a')['href']).to eq('http://example.com/coc')
-          end
         end
       end
 
@@ -178,11 +126,6 @@ describe 'creating or editing an rsvp' do
     end
 
     describe "a new learn rsvp" do
-      it "should not allow students to have 'No preference'" do
-        visit learn_new_event_rsvp_path(@event)
-        expect(page).not_to have_content no_preference_text
-      end
-
       describe "plus-one host toggle" do
         let(:plus_one_host_text) { "If you are not a member of this workshop's target demographic" }
 
@@ -215,9 +158,13 @@ describe 'creating or editing an rsvp' do
       context 'when event asks a custom question' do
         let(:custom_question) { 'What is your t-shirt size?' }
 
-        it 'diplays a field for the user to respond to the custom question' do
-          expect(page).to have_content custom_question
-          expect(page).to have_field('rsvp_custom_question_answer')
+        it 'allows the user to answer the custom question' do
+          fill_in_valid_volunteer_details
+          fill_in custom_question, with: 'Medium'
+
+          click_on 'Submit'
+
+          expect(Rsvp.last.custom_question_answer).to eq('Medium')
         end
       end
 
@@ -237,13 +184,6 @@ describe 'creating or editing an rsvp' do
       @user = create(:user)
       sign_in_as @user
       visit volunteer_new_event_rsvp_path(@event)
-    end
-
-    it "should not show teaching UI" do
-      expect(page).not_to have_content "What's your experience with teaching?"
-      expect(page).not_to have_content "Teaching"
-      expect(page).not_to have_content "TAing"
-      expect(page).not_to have_content "Do you have a class level preference?"
     end
 
     it "should require subject experience" do
---
+++ spec/features/events/rsvp/event_rsvp_coc_request_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe 'code of conduct checkbox for RSVPs' do
+  let(:coc_text) { 'I accept the Code of Conduct' }
+  let(:chapter) { create(:chapter) }
+
+  before do
+    @event = create(:event, chapter: chapter)
+    @user = create(:user)
+    sign_in_as @user
+  end
+
+  context 'for new records', js: true do
+    before do
+      visit volunteer_new_event_rsvp_path(@event)
+    end
+
+    it "requires code of conduct to be checked, and preserves checked-ness on error" do
+      expect(page).to have_content(coc_text)
+
+      expect(page).to have_button 'Submit', disabled: true
+      expect(page).to have_unchecked_field('coc')
+      check('coc')
+      expect(page).to have_button 'Submit', disabled: false
+
+      click_on 'Submit'
+
+      expect(page).to have_css('#error_explanation')
+      expect(page).to have_checked_field('coc')
+    end
+  end
+
+  context 'for existing records' do
+    let(:rsvp) { create(:rsvp, user: @user) }
+
+    it 'is not shown' do
+      visit edit_event_rsvp_path rsvp.event, rsvp
+      expect(page).to have_no_content(coc_text)
+    end
+  end
+
+  context 'when the organization has a different code of conduct' do
+    let(:organization) do
+      create(:organization, name: 'CoolBridge', code_of_conduct_url: 'http://example.com/coc')
+    end
+    let(:chapter) { create(:chapter, organization: organization) }
+
+    it 'shows a custom code of conduct URL' do
+      visit volunteer_new_event_rsvp_path(@event)
+      expect(page.find('label[for=coc] a')['href']).to eq('http://example.com/coc')
+    end
+  end
+end
