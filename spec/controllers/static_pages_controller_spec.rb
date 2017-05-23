5ab4c604c81624518c459e31f95c87bd564be951
LauraKirby <laurakirby26@gmail.com>
2017-05-23 13:31:22 -0700

    547 Add controller tests for 'static_pages_controller' and feature tests for the 'style_guide' page

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe StaticPagesController do
+
+  describe "GET #style_guide" do
+    before do
+      get :style_guide
+    end
+
+    it 'resonds with a success message' do
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the 'style_guide' template" do
+      expect(response).to render_template(:style_guide)
+    end
+  end
+
+  describe "GET #about" do
+    before do
+      get :about
+    end
+
+    it 'resonds with a success message' do
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the 'about' template" do
+      expect(response).to render_template(:about)
+    end
+  end
+end
+
