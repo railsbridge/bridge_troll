4350e8712664eb0a3ca383100c562a429b31ba01
Sameer Siruguri <siruguri@gmail.com>
2019-04-28 11:12:55 -0700

    Bump to Ruby 2.6

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Level do
   it { should belong_to(:course) }
   it { should validate_presence_of(:num) }
-  it { should validate_inclusion_of(:num).in_array((1..5).to_a) }
+  it { should validate_inclusion_of(:num).in_array((1..5).to_a).with_message('Must be between 1 and 5') }
   it { should validate_presence_of(:color) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:level_description) }
