0cd71ea22fe0a4f7f6fa85412477d4b5277157ae
Michael Glass <me@mike.is>
2019-08-13 18:13:22 +0200

    Revert "Enable hsts preload header"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   force_ssl if: -> { Rails.env.production? }, unless: :allow_insecure?
-  before_action :redirect_to_host_url
 
   before_action do
     if current_user.try(:admin?)
@@ -42,19 +41,5 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:error] = "You are not authorized to perform this action."
     redirect_to(request.referer || root_path)
-  end
-
-  # we redirect to host url only after force_ssl to support hsts
-  def redirect_to_host_url
-    return unless Rails.env.production?
-    host_url = ENV['HOST_URL']
-    # don't redirect if we're already there
-    return if request.host.downcase.starts_with?(host_url.downcase)
-    # don't redirect if not ssl and not allow_insecure
-    if ssl?
-      redirect_to("https://#{host_url}#{request.fullpath}", status: :moved_permanently)
-    elsif allow_insecure?
-      redirect_to("http://#{host_url}#{request.fullpath}", status: :moved_permanently)
-    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-  config.ssl_options = { hsts: { preload: true } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
