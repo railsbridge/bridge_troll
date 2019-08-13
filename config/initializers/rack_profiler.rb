ff3d517cbafc78327173b03150d0fc881fc25887
Michael Glass <me@mike.is>
2019-08-13 19:22:27 +0200

    we use mini profiler in production, so don't only run it in dev

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development?
-  require "rack-mini-profiler"
+require "rack-mini-profiler"
 
-  # initialization is skipped so trigger it
-  Rack::MiniProfilerRails.initialize!(Rails.application)
-end
+# initialization is skipped so trigger it
+Rack::MiniProfilerRails.initialize!(Rails.application)
