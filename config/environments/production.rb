50253944c71b9afff8044bc7bf6e715f15d6b7f5
Michael Glass <me@mike.is>
2019-08-15 09:57:35 +0200

    ActionDispatch isn't inserted until after config is consumed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,15 +60,6 @@ Rails.application.configure do
     }
   }
 
-  # taken from https://github.com/tylerhunt/rack-canonical-host/issues/36#issuecomment-330813507
-  # to support HSTS, we want to redirect to SSL before redirecting to www
-  config.middleware.insert_after(
-    ActionDispatch::SSL,
-    Rack::CanonicalHost,
-    ENV['CANONICAL_HOST'],
-    cache_control: 'max-age=3600'
-  )
-
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
@@ -121,3 +112,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
 end
+
+# taken from https://github.com/tylerhunt/rack-canonical-host/issues/36#issuecomment-330813507
+# to support HSTS, we want to redirect to SSL before redirecting to www
+Rails.application.middleware.insert_after(
+  ActionDispatch::SSL,
+  Rack::CanonicalHost,
+  ENV['CANONICAL_HOST'],
+  cache_control: 'max-age=3600'
+)
