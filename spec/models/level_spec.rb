baa844c06bb96d098e56e14d65d63799de58228f
Michael Glass <me@mike.is>
2020-01-02 11:28:56 +0100

    auto-correct Layout/BlockAlignment cop

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -6,7 +6,7 @@ describe Level do
   it { should validate_inclusion_of(:num)
       .in_array((1..5).to_a)
       .with_message("Must be between 1 and 5")
-    }
+  }
   it { should validate_presence_of(:color) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:level_description) }
