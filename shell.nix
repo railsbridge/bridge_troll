ad6d3aba1f485d2485ed00d54aec33b08e4c763a
Michael Glass <me@mike.is>
2021-05-04 11:56:14 +0200

    add nokogiri dependencies to nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,12 @@
 let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
-in pkgs.mkShell { buildInputs = [ pkgs.ruby_2_7 ]; }
+in pkgs.mkShell {
+  buildInputs = [
+    # for nokogiri
+    pkgs.zlib
+    pkgs.libiconv
+    #
+    pkgs.ruby_2_7
+  ];
+}
