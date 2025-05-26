84d50faafb1f8b1f969e830b8e74332b30de344e
Michael Glass <me@mike.is>
2025-05-26 12:50:16 +0200

    fix jasmine tests by removing puma rackup config

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,6 @@ threads threads_count, threads_count
 
 preload_app! unless worker_count == 1
 
-rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
