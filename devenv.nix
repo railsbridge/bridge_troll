5246f8dc9e9fac7edc15281186b826109069f5d5
Michael Glass <me@mike.is>
2025-05-26 12:49:37 +0200

    move from shell.nix to devenv.sh

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    chromedriver
+    postgresql.dev
+    libffi
+    openssl.dev
+    openldap
+    krb5
+  ];
+
+  env.LD_LIBRARY_PATH = lib.makeLibraryPath [
+    pkgs.krb5
+    pkgs.openldap
+  ];
+
+  enterShell = ''
+    git --version
+    ruby --version
+    bundle
+  '';
+
+  services.postgres.enable = true;
+
+  languages.ruby = {
+    enable = true;
+    versionFile = ./.ruby-version;
+    bundler.enable = false;
+  };
+
+  languages.javascript = {
+    enable = true;
+    yarn.enable = true;
+    yarn.install.enable = true;
+  };
+
+  git-hooks.hooks = {
+    shellcheck.enable = true;
+    nixfmt-rfc-style.enable = true;
+  };
+}
