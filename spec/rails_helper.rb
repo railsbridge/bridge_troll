ea147acdcf0dd967284593edafea349d346a274e
Michael Glass <me@mike.is>
2025-05-28 16:07:07 +0200

    require spec_helper sooner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
 
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
-require 'spec_helper'
 require 'rspec/rails'
 require 'rails-controller-testing'
 # Add additional requires below this line. Rails is not loaded until this point!
