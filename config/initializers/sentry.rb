ad464fc06e858fb117d405b93ae16dfb30c1bdb8
Michael Glass <me@mike.is>
2025-05-28 15:01:08 +0200

    only initialize sentry in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  # get breadcrumbs from logs
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  # Add data like request headers and IP for users, if applicable;
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
-  config.send_default_pii = true
+if Rails.env.production?
+  Sentry.init do |config|
+    # get breadcrumbs from logs
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    # Add data like request headers and IP for users, if applicable;
+    # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+    config.send_default_pii = true
+  end
 end
