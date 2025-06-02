f532b196d00407f539c2c9070a393b06cf5d8f6a
Michael Glass <me@mike.is>
2025-06-02 15:12:03 +0200

    fix /organizations considering online courses

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -14,6 +14,7 @@ class OrganizationsController < ApplicationController
     @chapter_locations = Event
                          .includes(:location, :chapter)
                          .where(id: chapter_last_event_ids)
+                         .select { |e| e.location.present? }
                          .map { |e| ChapterEventLocation.new(e) }
   end
 
